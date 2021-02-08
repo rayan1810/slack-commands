449b1333a2a723ac37c1008b59691faf4db7a166
Rohit Singh <rohits@geekyants.com>
2021-02-08 11:42:48 +0530

    add random messsages for weekdays and weekends

--- src/api.js
+++ src/api.js
@@ -7,9 +7,28 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  const messages = [
+    "Signing off for the day, Good night!",
+    "Wrapping up, Good night everyone.",
+    "Signing off , Good Night.",
+    "Wrapping up for the day. Good night.",
+  ];
+  const weekend_messages = [
+    " and a Happy Weekend everyone 🏖️",
+    " , Happy Weekend 🌴",
+    " and a Happy Weekend 😄",
+    " and a Happy Weekend everyone 🏝️",
+  ];
+  const randomInd = Math.floor(Math.random() * 3);
+  let curr_text_msg = messages[randomInd];
+  let today = new Date();
+  if (today.getDay() == 6 || today.getDay() == 0) {
+    curr_text_msg = curr_text_msg.slice(0, -1) + weekend_messages[randomInd];
+  }
+
   res.json({
     response_type: "in_channel",
-    text: "Signing off , Good Night",
+    text: curr_text_msg,
   });
 });
 
